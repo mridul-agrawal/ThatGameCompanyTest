e82691b527e1be9274fdd3cfcd13e13781dd5a50
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-03-01 16:25:22 +0530

    Completed the game loop for problem statement 1

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -2,30 +2,41 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GridManager : MonoBehaviour
 {
     public int rows;
     public int cols;
     public int AOI;
-    [SerializeField]
-    private GameObject Row;
-    [SerializeField]
-    private Tile Tile;
-    [HideInInspector]
-    public Tile[,] Grid;
+    [SerializeField] private GameObject Row;
+    private List<GameObject> Rows;
+    [SerializeField] private Tile Tile;
+    [HideInInspector] public Tile[,] Grid;
+
+    [SerializeField] private GameObject ResetPanel;
+    [SerializeField] private Slider rowSlider;
+    [SerializeField] private TextMeshProUGUI rowSliderTXT;
+    [SerializeField] private Slider colSlider;
+    [SerializeField] private TextMeshProUGUI colSliderTXT;
+    [SerializeField] private Slider aoiSlider;
+    [SerializeField] private TextMeshProUGUI aoiSliderTXT;
 
     private void Start()
     {
-        Grid = new Tile[rows, cols];
+        Rows = new List<GameObject>();
         GenerateGrid();
+        AssignListeners();
     }
 
     public void GenerateGrid()
     {
-        for(int row=0; row<rows; row++)
+        Grid = new Tile[rows, cols];
+        for (int row=0; row<rows; row++)
         {
             var newRow = Instantiate(Row, transform.position, Quaternion.identity);
+            Rows.Add(newRow);
             newRow.transform.SetParent(gameObject.transform);
 
             for(int col=0; col<cols; col++)
@@ -37,11 +48,25 @@ public class GridManager : MonoBehaviour
                 newTile.GenerateTileNo(cols);
                 newTile.GenerateTileColor();
                 newTile.OnClickTile += OpenTiles;
-                //newTile.GetComponent<Tile>().OpenTile();
 
                 Grid[row,col] = newTile.GetComponent<Tile>();
             }
         }
+    }
+
+    private void AssignListeners()
+    {
+        rowSlider.onValueChanged.AddListener((v) => {
+            rowSliderTXT.text = v.ToString();
+        });
+
+        colSlider.onValueChanged.AddListener((v) => {
+            colSliderTXT.text = v.ToString();
+        });
+
+        aoiSlider.onValueChanged.AddListener((v) => {
+            aoiSliderTXT.text = v.ToString();
+        });
     }
 
     public void OpenTiles(int row, int col)
@@ -58,6 +83,27 @@ public class GridManager : MonoBehaviour
                 Grid[row + a, col + b].OnClickTile -= OpenTiles;
             }
         }
+    }
+
+    public void CreateNewGrid()
+    {
+        foreach(GameObject row in Rows)
+        {
+            Destroy(row);
+        }
+
+        rows = (int)rowSlider.value;
+        cols = (int)colSlider.value;
+        AOI = (int)aoiSlider.value;
+
+        GenerateGrid();
+
+        ToggleResetPanel(false);
+    }
+
+    public void ToggleResetPanel(bool isActive)
+    {
+        ResetPanel.SetActive(isActive);
     }
 
 }
